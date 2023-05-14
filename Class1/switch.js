099a5eeb0210c1539f24d6cbd6fa03dd73d655f6
Nehal khatiwada <nehalkhatiwada2542@gmail.com>
2023-05-14 09:46:56 +0545

    js

---
+++ Class1/switch.js
@@ -0,0 +1,21 @@
+let obtained_mark =66;
+if(obtained_mark >=70)
+{
+ console.log("Distinction");  
+ 
+}
+else if (obtained_mark >= 60 && obtained_mark < 70)
+{
+    console.log("First class");
+}
+else if (obtained_mark >= 50 && obtained_mark < 60)
+{
+    console.log("Second  class");
+}
+else if (obtained_mark >= 40 && obtained_mark < 50)
+{
+    console.log("Third class");
+}
+else{
+    console.log("Fail");
+}
